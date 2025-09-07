d69f70ce1e2e7cae3b233c381859865453897e65
rameshuv <get.ramesh.uv@gmail.com>
2025-09-07 21:18:15 +0530

    Add missing slug translation

--- includes/helpers.php
+++ includes/helpers.php
@@ -169,6 +169,7 @@ if ( ! function_exists( 'bhg_get_default_translations' ) ) {
 			'label_id'                                     => 'ID',
 			'label_key'                                    => 'Key',
 			'label_name'                                   => 'Name',
+			'slug'                                         => 'Slug',
 			'label_winners'                                => 'Winners',
 			'label_title_content'                          => 'Title/Content',
 			'label_placement'                              => 'Placement',
